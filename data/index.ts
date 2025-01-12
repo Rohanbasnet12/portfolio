6d0caff43d1ca131a5d6c5653c96e4742c0bd56d
Rohanbasnet12 <rohanbmyname@gmail.com>
2025-01-12 16:55:51 +0530

    Removing /data from gitignore file

---
+++ data/index.ts
@@ -0,0 +1,115 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "",
+    spareImg: "/b1.jpg",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/grid.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Full Stack PERN application",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "NoteHive",
+    des: "NoteHive is a robust full-stack note-taking application designed to keep your thoughts, tasks, and ideas organized.",
+    img: "/noteHive.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+    link: "https://github.com/Rohanbasnet12/NoteHive",
+  },
+  {
+    id: 2,
+    title: "shopWease",
+    des: "shopWease is a modern PERN (PostgreSQL, Express, React, Node.js) stack e-commerce platform designed for effortless online shopping.",
+    img: "/shopWease.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://github.com/Rohanbasnet12/shopWease",
+  },
+  {
+    id: 4,
+    title: "Voice-Text",
+    des: "Voice to Text is an nextJS project, which allows users to convert their voice to text!",
+    img: "/Voice to text .png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "https://voice-search-alpha.vercel.app/",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/Rohanbasnet12/",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url: "https://x.com/RohanBa52433349",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/rohan-basnet-131841121/",
+  },
+];
