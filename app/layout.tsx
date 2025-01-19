a1b3492c09ddc570b4e6f7706d5f8cb4cef004f1
Rohanbasnet12 <rohanbmyname@gmail.com>
2025-01-19 18:38:32 +0530

    Creating fully new folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
-import { ThemeProvider } from "./theme-provider";
+import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Rohan's Portfolio",
-  description:
-    "Moder & Minimalist Portfolio created using NextJS, framer and tailwindCSS",
+  title: "Adrian's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
 };
 
 export default function RootLayout({
@@ -17,7 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,12 @@
-"use client";
 import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
-import Image from "next/image";
-
 const Footer = () => {
-  const currentYear: number = new Date().getFullYear();
-
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
+      {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
@@ -28,7 +24,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:rohanbmyname@gmail.com">
+        <a href="mailto:contact@jsmastery.pro">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
@@ -38,7 +34,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © {currentYear} Rohan basnet
+          Copyright © 2024 Adrian Hajdin
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
@@ -47,13 +43,7 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <Image
-                src={info.img}
-                alt="icons"
-                width={20}
-                height={20}
-                onClick={() => window.open(info.url, "_blank")}
-              />
+              <img src={info.img} alt="icons" width={20} height={20} />
             </div>
           ))}
         </div>
